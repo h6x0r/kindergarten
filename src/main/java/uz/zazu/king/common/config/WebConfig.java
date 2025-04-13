356d8cb82d4c82ddc6bd50ca7ac9071904eafdbf
Islom Domlajonov <islom.domlajonov@octo.uz>
2025-04-13 20:47:43 +0500

    Adjust CORS settings based on dev flag configuration.
    Added a new `dev` flag to dynamically configure CORS settings. In development, all origins and methods are allowed, whereas in production, stricter rules apply. Updated `application.yml` to include the `dev` flag.

--- src/main/java/uz/zazu/king/common/config/WebConfig.java
+++ src/main/java/uz/zazu/king/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package uz.zazu.king.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,13 +8,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${flag.dev}")
+    private boolean dev;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/api/**")
-                .allowedOrigins("http://localhost:3001")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        if (dev) {
+            registry
+                    .addMapping("/api/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        } else {
+            registry
+                    .addMapping("/api/**")
+                    .allowedOrigins("http://localhost:3001")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
+
     }
 }
