be33aa6682838b0a89b0250965e1d00ba243242e
Islom Domlajonov <islom.domlajonov@octo.uz>
2025-04-13 21:15:57 +0500

    Simplify CORS configuration by removing environment-specific logic.
    The CORS setup is now unified to allow all origins and methods, eliminating development-specific and environment-dependent configurations. Removed the `dev` flag from configuration files and code as it is no longer necessary.

--- src/main/java/uz/zazu/king/common/config/WebConfig.java
+++ src/main/java/uz/zazu/king/common/config/WebConfig.java
@@ -1,6 +1,5 @@
 package uz.zazu.king.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,27 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${flag.dev}")
-    private boolean dev;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        if (dev) {
-            registry
-                    .addMapping("/api/**")
-                    .allowedOrigins("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("Content-Type", "Authorization", "Accept", "X-Requested-With", "Origin")
-                    .exposedHeaders("Authorization")
-                    .allowCredentials(true);
-        } else {
-            registry
-                    .addMapping("/api/**")
-                    .allowedOrigins("http://localhost:3001")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-        }
-
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization", "Accept", "X-Requested-With", "Origin")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
     }
 }
